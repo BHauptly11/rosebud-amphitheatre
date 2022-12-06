4c21441064140022bf05993ab7b7b78b38268d3c
Misha Tuesday <mishatuesday@gmail.com>
2022-12-06 10:52:57 -0500

    add routes & component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Login />
+      <Routes>
+        <Route exact path="/" element={<ConcertList />} />
+        <Route path="/concessions" element={<ConcessionsList />} />
+        <Route path="/newconcert" element={<NewConcertForm />} />
+        <Route path="/newconcession" element={<NewConcessionForm />} />
+        <Route path="signup" element={<NewUserSignup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
