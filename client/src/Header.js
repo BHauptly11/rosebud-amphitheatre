12c039cb18d84b54ee80eba4c4bdeedd64cf2655
Misha Tuesday <mishatuesday@gmail.com>
2022-12-07 16:19:09 -0500

    this is actually header, not authorization

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,7 +1,15 @@
-
+import {NavLink} from 'react-router-dom'
 
 function Header () {
-    return (<h1>Rosebud Amphitheater</h1>)
+    return (
+    <div>
+        <h1>Rosebud Amphitheater</h1>
+        <NavLink to={`/`}>Concerts</NavLink>
+        <NavLink to={`/concessions`}>Concessions</NavLink>
+        <NavLink to={`/newband`}>Add Band</NavLink>
+        <NavLink to={`/newconcert`}>Add Concert</NavLink>
+    </div>
+    )
 }
 
 export default Header
